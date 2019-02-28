b5fe6e978a791838b31cf94fef618ec8ef03e9d4
AICProjects <32740334+AICProjects@users.noreply.github.com>
2019-02-28 13:30:36 -0500

    Create powerallrg.ps1

---
+++ powerallrg.ps1
@@ -0,0 +1,18 @@
+#Connect to Azure if needed
+#az connect
+
+#Define Resource group
+$RG_name="RG-SQLEncryption"
+
+#Define the action Accepted actions are start and stop
+$vm_action="stop"
+
+#Show me the vms in the resource group
+Write-Host "These are the affected vms.They are in Resource Group " $RG_name -ForegroundColor DarkCyan
+az vm list -g $RG_name --query "[].id" -o tsv
+
+#Display the action that will take place
+Write-Host "A" $vm_action "action is being performed on the vms" -ForegroundColor DarkBlue
+
+#power on/off the list of vms in the resource group
+az vm $vm_action --ids $(az vm list -g $RG_name --query "[].id" -o tsv)
